1f206f47b5c33dfe2f963d4ee07bf5f3c28cfc30
David García Garzón <voki@canvoki.net>
2015-09-16 15:14:31 +0200

    Added tipus

--- distribucio_de_socies.sql
+++ distribucio_de_socies.sql
@@ -1,6 +1,11 @@
-(SELECT provincia AS nombre, count(provincia)  AS recuento
+(
+SELECT
+    'PROVINCE' AS tipus,
+    provincia AS nombre,
+    count(provincia)  AS recuento
 FROM(
-	SELECT adreces.id, 
+	SELECT
+        adreces.id, 
 		adreces.partner_id, 
 		municipi.name AS municipi, 
 		comarca.name AS comarca,
@@ -8,32 +13,49 @@ FROM(
 		comunitat.name AS comunitat_autonoma,
 		pais.name AS pais
 		
-	FROM res_partner_address AS adreces
+	FROM
+        res_partner_address AS adreces
 	LEFT JOIN (
-		SELECT dades_inicials.partner_id, MIN(dades_inicials.id) AS id_unic
-		FROM res_partner_address as dades_inicials
-		WHERE dades_inicials.active
-		group by dades_inicials.partner_id
+		SELECT
+            dades_inicials.partner_id,
+            MIN(dades_inicials.id) AS id_unic
+		FROM
+            res_partner_address as dades_inicials
+		WHERE
+            dades_inicials.active
+		GROUP BY dades_inicials.partner_id
 		) as dades_inicials ON dades_inicials.id_unic = adreces.id
-	LEFT JOIN res_partner_category_rel AS category ON category.partner_id = dades_inicials.partner_id
-	LEFT JOIN res_municipi AS municipi ON municipi.id = adreces.id_municipi
-	LEFT JOIN res_comarca AS comarca ON comarca.id = municipi.comarca
-	LEFT JOIN res_country_state AS provincia ON provincia.id = adreces.state_id
-	LEFT JOIN res_comunitat_autonoma AS comunitat ON comunitat.id = provincia.comunitat_autonoma
-	LEFT JOIN res_country AS pais ON pais.id = adreces.country_id
+	LEFT JOIN res_partner_category_rel AS category
+        ON category.partner_id = dades_inicials.partner_id
+	LEFT JOIN res_municipi AS municipi
+        ON municipi.id = adreces.id_municipi
+	LEFT JOIN res_comarca AS comarca
+        ON comarca.id = municipi.comarca
+	LEFT JOIN res_country_state AS provincia
+        ON provincia.id = adreces.state_id
+	LEFT JOIN res_comunitat_autonoma AS comunitat
+        ON comunitat.id = provincia.comunitat_autonoma
+	LEFT JOIN res_country AS pais
+        ON pais.id = adreces.country_id
 
 	WHERE adreces.partner_id IS NOT NULL
 		AND category.category_id = 8
-		AND adreces.create_date < '2015-09-01') as taula_provincia
+		AND adreces.create_date < '2015-09-01'
+    ) AS taula_provincia
 GROUP BY provincia, comunitat_autonoma
 ORDER BY comunitat_autonoma, provincia
 )
 
 UNION ALL
 
-(SELECT comunitat_autonoma AS nombre, count(comunitat_autonoma) AS recuento
+(
+SELECT
+    'STATE' as tipus,
+    comunitat_autonoma AS nombre,
+    count(comunitat_autonoma) AS recuento
 FROM(
-	SELECT adreces.id, 
+	SELECT
+        adreces.id, 
 		adreces.partner_id, 
 		municipi.name AS municipi, 
 		comarca.name AS comarca,
@@ -47,10 +69,14 @@ FROM(
 		FROM res_partner_address as dades_inicials
 		WHERE dades_inicials.active
 		group by dades_inicials.partner_id
-		) as dades_inicials ON dades_inicials.id_unic = adreces.id
-	LEFT JOIN res_partner_category_rel AS category ON category.partner_id = dades_inicials.partner_id
-	LEFT JOIN res_municipi AS municipi ON municipi.id = adreces.id_municipi
-	LEFT JOIN res_comarca AS comarca ON comarca.id = municipi.comarca
+		) AS dades_inicials
+        ON dades_inicials.id_unic = adreces.id
+	LEFT JOIN res_partner_category_rel AS category
+        ON category.partner_id = dades_inicials.partner_id
+	LEFT JOIN res_municipi AS municipi
+        ON municipi.id = adreces.id_municipi
+	LEFT JOIN res_comarca AS comarca
+        ON comarca.id = municipi.comarca
 	LEFT JOIN res_country_state AS provincia ON provincia.id = adreces.state_id
 	LEFT JOIN res_comunitat_autonoma AS comunitat ON comunitat.id = provincia.comunitat_autonoma
 	LEFT JOIN res_country AS pais ON pais.id = adreces.country_id
@@ -63,7 +89,11 @@ ORDER BY taula_comunitat_autonoma.comunitat_autonoma)
 
 UNION ALL
 
-(SELECT comarca AS nombre, count(comarca) AS recuento
+(
+SELECT
+    'COUNTY' as tipus,
+    comarca AS nombre,
+    count(comarca) AS recuento
 FROM(
 	SELECT adreces.id, 
 		adreces.partner_id, 
@@ -97,7 +127,11 @@ ORDER BY comunitat_autonoma, comarca)
 
 UNION ALL
 
-(SELECT pais AS nombre, count(pais) AS recuento
+(
+SELECT
+    'COUNTRY' as tipus,
+    pais AS nombre,
+    count(pais) AS recuento
 FROM(
 	SELECT adreces.id, 
 		adreces.partner_id, 
