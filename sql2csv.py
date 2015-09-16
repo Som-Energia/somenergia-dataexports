77361890c1a731591aa7e70e854c14d52ed997af
David García Garzón <voki@canvoki.net>
2015-09-16 15:13:55 +0200

    sql2csv: loading a yaml with variables

--- sql2csv.py
+++ sql2csv.py
@@ -6,13 +6,24 @@ import config
 import dbutils
 import sys
 from consolemsg import step, error, fail, warn
+from namespace import namespace as ns
 
+
+step("Loading {}...".format(sys.argv[1]))
 with open(sys.argv[1]) as sqlfile:
     query = sqlfile.read()
 
+variables = ns()
+if len(sys.argv)>=3:
+    step("Loading variables...".format(sys.argv[2]))
+    variables = ns.load(sys.argv[2])
+    warn(variables.dump())
+
+step("Connecting to the database...")
 db = psycopg2.connect(**config.psycopg)
+
 with db.cursor() as cursor :
-    cursor.execute(query)
+    cursor.execute(query, variables)
     print dbutils.csvTable(cursor)
 
 
