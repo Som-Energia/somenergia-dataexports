63d2fe0d5d5dee99fd91659f5d0b9e89062f052f
David García Garzón <voki@canvoki.net>
2015-09-16 13:17:58 +0200

    Color grading and relative to population alternate

--- mapa_socis.py
+++ mapa_socis.py
@@ -3,6 +3,11 @@
 
 from namespace import namespace as ns
 import math
+from consolemsg import step
+
+month = 7
+year = 2014
+
 
 def readCsvTable(filename):
     """Reads a tab separated csv file as an array of
@@ -30,11 +35,18 @@ def countBy(column, data, noneValue=None) :
     result={}
     for line in data:
         keyValue = line[column]
-        if keyValue is "None":
-            keyValue = None
+        if keyValue == 'None':
+            keyValue = noneValue
         previousCount = result.get(keyValue,0)
         result[keyValue] = previousCount + int(line.quants)
     return result
+
+def renderMap(filename, data):
+    with open("MapaSocios-template.svg") as svgTemplateFile:
+        svgTemplate = svgTemplateFile.read()
+
+    with open(filename,'w') as svgAbsolut:
+        svgAbsolut.write(svgTemplate.format(**output))
 
 
 populationPerCCAA = dict(
@@ -48,81 +60,67 @@ socisPerCCAA = countBy('codi_ccaa', distribucioSocis, noneValue='00')
 socisPerProvincia = countBy('codi_provincia', distribucioSocis, noneValue='00')
 socisPerMunicipi = countBy('codi_ine', distribucioSocis, noneValue='00000')
 
-totalSocis = sum(value for value in socisPerCCAA.values())
-maxSocis = max(value for value in socisPerCCAA.values())
-minSocis = 0 #min(value for value in socisPerCCAA.values())
-
-colors = [
-    '#EE0',
-    '#DD0',
-    '#CC0',
-    '#BB0',
-    '#AA0',
-    '#990',
-    '#880',
-    '#770',
-    '#660',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-    '#440',
-]
-
+relativeSocisPerCCAA = dict(
+    (ccaa, socis*10000./populationPerCCAA[ccaa])
+    for ccaa, socis in socisPerCCAA.items()
+    )
 
 def mapColor(value, minValue, maxValue):
-    print value, minValue, maxValue
+    colors = [
+        '#EE0',
+        '#DD0',
+        '#CC0',
+        '#BB0',
+        '#AA0',
+        '#990',
+        '#880',
+        '#770',
+        '#660',
+        ] + [
+        '#440',
+        ]*15
     return colors[int((len(colors)-1)*(value-minValue)/(maxValue-minValue))]
 
 
-with open("MapaSocios-template.svg") as svgTemplateFile:
-    svgTemplate = svgTemplateFile.read()
+totalSocis = sum(value for value in socisPerCCAA.values())
+
+
+months = (
+    "Enero Febrero Marzo Abril Mayo Junio "
+    "Julio Septiembre Octubre Noviembre Diciembre"
+    ).split()
+
 
 output = ns(
-    month = 'Marzo',
-    year=2015,
+    month = months[month-1],
+    year = year,
     )
 
+step("Generant mapa amb valors absoluts")
+
+maxSocis = max(value for value in socisPerCCAA.values())
+minSocis = min(value for value in socisPerCCAA.values())
 for ccaa, population in populationPerCCAA.items():
     socis = socisPerCCAA.get(ccaa,0)
     output['number_'+ccaa] = socis
-    output['relative_'+ccaa] = '{:.2f}'.format(socis*10000./population)
-    output['percent_'+ccaa] = '{:.2f}'.format(socis*100./totalSocis)
+    output['percent_'+ccaa] = '{:.2f}%'.format(socis*100./totalSocis)
     output['color_'+ccaa] = mapColor(socis, minSocis, maxSocis)
+renderMap('SocisPerCCAA-absoluts.svg', output)
 
-print output
+step("Generant mapa amb valors relatius")
 
-with open('SocisPerCCAA-absoluts.svg','w') as svgAbsolut:
-    svgAbsolut.write(svgTemplate.format(**output))
+maxRelativeSocis = max(value for value in relativeSocisPerCCAA.values())
+minRelativeSocis = min(value for value in relativeSocisPerCCAA.values())
 
 for ccaa, population in populationPerCCAA.items():
-    output['number_'+ccaa] = output['relative_'+ccaa]
+    relativeSoci = relativeSocisPerCCAA.get(ccaa,0)
+    output['number_'+ccaa] = '{:.2f}'.format(relativeSoci)
+    output['color_'+ccaa] = mapColor(
+        relativeSoci, minRelativeSocis, maxRelativeSocis)
+    output['percent_'+ccaa] = ''
+renderMap('SocisPerCCAA-relatius.svg', output)
 
-with open('SocisPerCCAA-relatius.svg','w') as svgRelatiu:
-    svgRelatiu.write(svgTemplate.format(**output))
-    
+ 
 
 
 
