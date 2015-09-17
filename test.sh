c7958924685c55985774894ab1ca45a445bba13c
David García Garzón <voki@canvoki.net>
2015-09-17 12:07:34 +0200

    clearing result files if ok, printing it if ko

--- test.sh
+++ test.sh
@@ -2,21 +2,22 @@
 
 ok() {
     echo -e '\033[32mOK\033[0m'
+    rm -f "$result"
 }
 ko() {
-    echo -e '\033[31mKO\033[0m'
+    echo -e '\033[31mKO\033[0m' See $1
 }
 step() {
     echo -e '\033[34;1m:: '$*'\033[0m'
 } 
-
 
 for data in 2015-02-01 2014-12-01 2014-11-01;
 do
     result=b2bdata/pere-$data-result.csv
     expect=${result/result.csv/expected.csv}
     step Running ./sql2csv.py distribucio_de_socies.sql --data "${data}"
-    ./sql2csv.py distribucio_de_socies.sql --data "${data}" > "$result" 2>&1 && diff "$expect" "$result" && ok || ko
+    rm -f "$result"
+    ./sql2csv.py distribucio_de_socies.sql --data "${data}" > "$result" 2>&1 && diff "$expect" "$result" && ok "$result" || ko "$result"
 done
 
 for data in 2015-02-01 2014-12-01 2014-11-01;
@@ -24,7 +25,8 @@ do
     result=b2bdata/distribucio-$data-result.csv
     expect=${result/result.csv/expected.csv}
     step Running ./distribucio_de_socies.py "${data}"
-    ./distribucio_de_socies.py "${data}" > "$result" 2>&1 && diff "$expect" "$result" && ok || ko
+    rm -f "$result"
+    ./distribucio_de_socies.py "${data}" > "$result" 2>&1 && diff "$expect" "$result" && ok "$result" || ko "$result"
 done
 
 #./mchimp_generationsocis-sql.py >res 2>reserr; diff ref res && diff referr reserr && ok || ko
