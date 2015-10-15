dcfe68512c4dd3e16bd7103312fadec71d4918fa
David García Garzón <voki@canvoki.net>
2015-10-15 19:38:52 +0200

    changes asked from Francesc wip

--- mchimp_generationsocis-sql.py
+++ mchimp_generationsocis-sql.py
@@ -18,69 +18,91 @@ def ambPuntDeMilers(numero) :
 db = psycopg2.connect(**config.psycopg)
 with db.cursor() as cursor :
     cursor.execute("""\
-        SELECT * FROM (
-        SELECT DISTINCT ON (sub.soci_id)
-            sub.soci_id as soci_id,
-            sub.name AS name,
-            sub.nsoci AS nsoci,
-            sub.nif AS nif,
-            sub.lang AS lang,
-            sub.consumannual AS consumannual,
-            sub.ncontractes AS ncontractes,
-            address.email,
+        SELECT
+            soci_id,
+            name,
+            nsoci,
+            nif,
+            lang,
+            consumannual,
+            ncontractes,
+            email,
+            already_invested IS NOT NULL AS already_invested,
             FALSE
         FROM (
-            SELECT
-                soci.id AS soci_id,
-                soci.name AS name,
-                soci.ref AS nsoci,
-                soci.vat AS nif,
-                soci.lang AS lang,
-                SUM(cups.conany_kwh) AS consumannual,
-                COUNT(cups.conany_kwh) AS ncontractes,
+            SELECT DISTINCT ON (sub.soci_id)
+                sub.soci_id as soci_id,
+                sub.name AS name,
+                sub.nsoci AS nsoci,
+                sub.nif AS nif,
+                sub.lang AS lang,
+                sub.consumannual AS consumannual,
+                sub.ncontractes AS ncontractes,
+                address.email,
                 FALSE
-            FROM res_partner AS soci
+            FROM (
+                SELECT
+                    soci.id AS soci_id,
+                    soci.name AS name,
+                    soci.ref AS nsoci,
+                    soci.vat AS nif,
+                    soci.lang AS lang,
+                    SUM(cups.conany_kwh) AS consumannual,
+                    COUNT(cups.conany_kwh) AS ncontractes,
+                    FALSE
+                FROM res_partner AS soci
+                LEFT JOIN
+                    giscedata_polissa AS pol ON (
+                        pol.titular = soci.id OR
+                        pol.pagador = soci.id
+                        )
+                LEFT JOIN 
+                    giscedata_cups_ps AS cups ON cups.id = pol.cups
+                LEFT JOIN
+                    res_partner_category_rel AS cat ON cat.partner_id = soci.id
+                WHERE
+                    cat.category_id = 8 AND
+                    soci.active AND
+                    pol.active AND
+                    pol.state = 'activa' AND
+                    cups.active AND
+                    TRUE
+                GROUP BY
+                    soci.id
+                ORDER BY
+                    soci.id ASC
+            ) AS sub
             LEFT JOIN
-                giscedata_polissa AS pol ON (
-                    pol.titular = soci.id OR
-                    pol.pagador = soci.id
-                    )
-            LEFT JOIN 
-                giscedata_cups_ps AS cups ON cups.id = pol.cups
-            LEFT JOIN
-                res_partner_category_rel AS cat ON cat.partner_id = soci.id
+                res_partner_address AS address ON (address.partner_id = sub.soci_id)
             WHERE
-                cat.category_id = 8 AND
-                soci.active AND
-                pol.active AND
-                pol.state = 'activa' AND
-                cups.active AND
+                address.active AND
+                address.email IS NOT NULL AND
+                address.email != '' AND
                 TRUE
             GROUP BY
-                soci.id
-            ORDER BY
-                soci.id ASC
-        ) AS sub
-        LEFT JOIN
-            res_partner_address AS address ON (address.partner_id = sub.soci_id)
+                sub.soci_id,
+                sub.name,
+                sub.nsoci,
+                sub.nif,
+                sub.lang,
+                sub.consumannual,
+                sub.ncontractes,
+                address.email,
+                TRUE
+        ) AS result
+        LEFT JOIN (
+            SELECT DISTINCT
+                partner_id AS already_invested
+            FROM payment_line AS line
+            LEFT JOIN
+                payment_order AS remesa ON remesa.id = line.order_id 
+            WHERE
+                remesa.mode = 19
+            ) AS investments ON already_invested = soci_id 
         WHERE
-            address.active AND
-            address.email IS NOT NULL AND
-            address.email != '' AND
             TRUE
-        GROUP BY
-            sub.soci_id,
-            sub.name,
-            sub.nsoci,
-            sub.nif,
-            sub.lang,
-            sub.consumannual,
-            sub.ncontractes,
-            address.email,
-            TRUE
-        ) as result
         ORDER BY
-            result.name ASC
+            name ASC
             
     ;
 """)
@@ -103,6 +125,9 @@ with db.cursor() as cursor :
         'Recommended shares',
         'Covered use',
         'Recommended investment',
+        'Already invested',
+        'Unknown use',
+        'Small use',
         ])
 
 
@@ -113,11 +138,12 @@ with db.cursor() as cursor :
             if totalUse is None:
                 warn("Soci {} amb consum null".format(
                     line.nsoci))
-                continue
+                totalUse = 0
+#                continue
 
             if totalUse * recommendedPercent < shareUse * 100 :
                 error("El soci {} no te prou consum ({})".format(line.nsoci, totalUse))
-                continue
+#                continue
 
             if line.nif[:2] != 'ES':
                 warn("Soci amb un VAT code no espanyol: {}".format(line.nif[:2]))
@@ -145,6 +171,9 @@ with db.cursor() as cursor :
                 ambPuntDeMilers(recommendedShares),
                 ambPuntDeMilers(recommendedShares * shareUse),
                 ambPuntDeMilers(recommendedInvestment),
+                1 if line.already_invested else 0,
+                1 if totalUse is None else 0,
+                1 if totalUse * recommendedPercent < shareUse * 100 else 0,
                 ])
         except Exception as e:
             import traceback
