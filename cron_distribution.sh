e5cfc85368d342b6608392e872dd2ab3c6e0f51d
David García Garzón <voki@canvoki.net>
2016-01-12 17:08:58 +0100

    changes

--- cron_distribution.sh
+++ cron_distribution.sh
@@ -15,8 +15,6 @@ do
 done < recipients-distribucio
 )
 
-echo $TOOPTIONS
-
 
 today=$(date -I)
 IFS='-' read -r year month day <<< "$today" # split date
@@ -31,13 +29,40 @@ step "Generating reports at $year-$month-$day"
 ./distribucio_de_socies.py "$year-$month-$day" > distribucion-socias-$year-$month-$day-detalle.csv || die
 ./sql2csv.py distribucio_de_socies.sql --data "$year-$month-$day" > distribucion-socias-$year-$month-$day-agregado.csv || die
 
+step "Sending results..."
+
+TEXTOK="
+# Distribución de socixs ($year-$month-$day)
+
+Se adjuntan los ficheros con los mapas y las hojas de cálculo de la
+**distribución de socixs** a dia **$year-$month-$day**.
+
+Datos:
+
+- detalle por municipios
+- agregado por CCAA's, Provincias...
+
+Mapas:
+
+- png (rasterizado) i svg (vectorial editable)
+- Per CCAA i Per Provincies
+- relatius a la poblacio i en nombres absoluts
+
+
+[Más información]
+
+[Más información]:http://somenergia.coop
+
+"
+
 ./sendmail.py \
     --subject "Distribución socixs $year-$month-$day" \
     $TOOPTIONS \
     --from sistemes@somenergia.coop \
     --cc david.garcia@somenergia.coop \
     --replyto david.garcia@somenergia.coop \
-    --body "Adjuntos van los ficheros de la distribución de socixs a día $year-$month-$day" \
+    --format md \
+    --style somenergia.css \
     "distribucion-socias-$year-$month-$day-detalle.csv" \
     "distribucion-socias-$year-$month-$day-agregado.csv" \
     "SocisPerCCAA-absoluts-$year-$month.svg" \
@@ -48,7 +73,7 @@ step "Generating reports at $year-$month-$day"
     "SocisPerProvincies-absoluts-$year-$month.png" \
     "SocisPerProvincies-relatius-$year-$month.svg" \
     "SocisPerProvincies-relatius-$year-$month.png" \
+    --body "$TEXTOK" \
     || die
-    
 
 
